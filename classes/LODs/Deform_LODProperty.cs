44bd473e187a4d9018d192254585c7c6fe55c47b
notherey <64904218+notherey@users.noreply.github.com>
2021-12-02 13:47:31 +0100

    Delete Deform_LODProperty.cs

--- classes/LODs/Deform_LODProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Deform_LODProperty : MonoBehaviour, ILodable
-{
-    public void OnLODUpdate(int currentGroup, Transform[] groups)
-    {
-        Debug.LogWarning("DeformLODProp ~ not implemented");
-    }
-}
